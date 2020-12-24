2dc3448e95a8d4277a5431c894d9ee0327c1cd22
shahid raza <55103610+shahid-raza72@users.noreply.github.com>
2020-12-24 12:44:13 +0530

    Add files via upload

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Inputstudent from './components/Inputstudent'
+import Liststudent from './components/Liststudent'
+function App() {
+  return (
+    <div>
+      <Inputstudent/>
+      <Liststudent/>
+    </div>
+  );
+}
+
+export default App;
